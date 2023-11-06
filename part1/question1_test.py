f3432c6705713c8ee360cad442affd7cc88ee8c7
Saul Andres Menjura Ovalle <55222565+andresovalle8@users.noreply.github.com>
2023-11-05 22:13:08 -0500

    Update question1_test.py
    Solution question 1

--- part1/question1_test.py
+++ part1/question1_test.py
@@ -5,3 +5,18 @@ def test_get_city_weather():
   assert get_city_weather("Quito") == "22 degrees and sunny"
 
   assert get_city_weather("New York") == "14 degrees and rainy"
+
+
+def get_city_weather(city):
+  
+    temperature = get_city_temperature(city)
+
+    if city == "Quito":
+        sky_condition = "sunny"
+    elif city == "New York":
+        sky_condition = "rainy"
+    else:
+        sky_condition = "unknown"
+
+    return str(temperature) + " degrees and " + sky_condition
+
